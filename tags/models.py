5e90f7862d30b81a69724b08737284bf74850fa0
yari <yarimedici@gmail.com>
2024-02-06 15:37:38 +0100

    Added created_at field to Tag model

--- tags/models.py
+++ tags/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=80, blank=False)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
