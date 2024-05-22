6270d0d801a340554c8c989c4cb161704b2320f8
Yari <yarimedici@gmail.com>
2024-05-22 16:44:33 +0200

    Added functional tests for multiple users and for localization

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,4 @@
 import unittest
-import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.translation import activate
@@ -19,14 +18,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
         options.add_argument("--headless")
         self.browser = webdriver.Firefox(options=options)
         self.browser.implicitly_wait(10)
+        self.browser.get(self.live_server_url)
 
-        # Sets up a logged-in user for testing.
-        # We're creating and logging in a user to
-        # facilitate testing scenarios where a logged-in
-        # user is required, such as creating a post and
-        # liking it. The logged-in user is made available
-        # to all tests within the class.
-        self.browser.get(self.live_server_url + reverse('create_account'))
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_single_user_can_create_a_post(self):
+        self.browser.find_element(By.CLASS_NAME, 'signup-button').click()
         email_input = self.browser.find_element(By.NAME, 'email')
         email_input.send_keys('test@test.com')
         username_input = self.browser.find_element(By.NAME, 'username')
@@ -43,41 +41,132 @@ class NewVisitorTest(StaticLiveServerTestCase):
         password_input.send_keys('testpassword')
         self.browser.find_element(By.XPATH, '//input[@value="login"]').click()
 
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_single_user_can_create_a_post(self):
-        self.browser.get(self.live_server_url + reverse('create'))
-
-        # Create article
+        # Create post
+        self.browser.find_element(By.CLASS_NAME, 'create-button').click()
         title_input = self.browser.find_element(By.NAME, 'title')
         title_input.send_keys('test title')
         slug_input = self.browser.find_element(By.NAME, 'slug')
         slug_input.send_keys('test-title')
-        
         self.browser.execute_script("tinyMCE.activeEditor.selection.setContent('test content ')")
-
         source_input = self.browser.find_element(By.NAME, 'source')
         source_input.send_keys('http://www.test.com')
         tag_input = self.browser.find_element(By.NAME, 'tag')
         tag_input.send_keys('test_tag')
-        #self.browser.find_element(By.XPATH, '//input[@value="Submit"]').click()
         self.browser.find_element(By.CLASS_NAME, 'create-btn').click()
         
         # Check if title in home page
         title_text = self.browser.find_element(By.CLASS_NAME, 'home-title').text
-        self.assertIn('test title', title_text)
+        self.assertEqual('test title', title_text)
 
         # Check if author is set correctly
         author_text = self.browser.find_element(By.CLASS_NAME, 'home-author').text
-        self.assertIn('test@test.com', author_text)
+        self.assertEqual('test@test.com', author_text)
 
         # Check if the search bar works correctly
         search_text = self.browser.find_element(By.CLASS_NAME, 'search-bar')
         search_text.send_keys('test-tag')
         self.browser.find_element(By.CLASS_NAME, 'search-bar').click()
         title_text = self.browser.find_element(By.CLASS_NAME, 'home-title').text
-        self.assertIn('test title', title_text)
+        self.assertEqual('test title', title_text)
 
-    @unittest.skip(reason='Skipping for now')
-    def test_multiple_user_can_create_posts(self): ...
+    def test_multiple_user_can_create_posts(self):
+        # create first user
+        self.browser.find_element(By.CLASS_NAME, 'signup-button').click()
+        email_input = self.browser.find_element(By.NAME, 'email')
+        email_input.send_keys('test@test.com')
+        username_input = self.browser.find_element(By.NAME, 'username')
+        username_input.send_keys('test@test.com')
+        password1_input = self.browser.find_element(By.NAME, 'password1')
+        password1_input.send_keys('testpassword')
+        password2_input = self.browser.find_element(By.NAME, 'password2')
+        password2_input.send_keys('testpassword')
+        self.browser.find_element(By.XPATH, '//button[@value="create_account"]').click()
+
+        # log first user in
+        username_input = self.browser.find_element(By.NAME, 'username')
+        username_input.send_keys('test@test.com')
+        password_input = self.browser.find_element(By.NAME, 'password')
+        password_input.send_keys('testpassword')
+        self.browser.find_element(By.XPATH, '//input[@value="login"]').click()
+
+        # create post --> first user is the author
+        self.browser.find_element(By.CLASS_NAME, 'create-button').click()
+        title_input = self.browser.find_element(By.NAME, 'title')
+        title_input.send_keys('test title')
+        slug_input = self.browser.find_element(By.NAME, 'slug')
+        slug_input.send_keys('test-title')
+        self.browser.execute_script("tinyMCE.activeEditor.selection.setContent('test content')")
+        source_input = self.browser.find_element(By.NAME, 'source')
+        source_input.send_keys('http://www.test.com')
+        tag_input = self.browser.find_element(By.NAME, 'tag')
+        tag_input.send_keys('test_tag')
+        self.browser.find_element(By.CLASS_NAME, 'create-btn').click()
+
+        # log first user out
+        self.browser.find_element(By.CLASS_NAME, 'user-dropdown').click()
+        self.browser.find_element(By.CLASS_NAME, 'logout-dropdown').click()
+
+        # create second user
+        self.browser.find_element(By.CLASS_NAME, 'signup-button').click()
+        email_input = self.browser.find_element(By.NAME, 'email')
+        email_input.send_keys('test2@test.com')
+        username_input = self.browser.find_element(By.NAME, 'username')
+        username_input.send_keys('test2@test.com')
+        password1_input = self.browser.find_element(By.NAME, 'password1')
+        password1_input.send_keys('testpassword2')
+        password2_input = self.browser.find_element(By.NAME, 'password2')
+        password2_input.send_keys('testpassword2')
+        self.browser.find_element(By.XPATH, '//button[@value="create_account"]').click()
+
+        # log second user in
+        username_input = self.browser.find_element(By.NAME, 'username')
+        username_input.send_keys('test2@test.com')
+        password_input = self.browser.find_element(By.NAME, 'password')
+        password_input.send_keys('testpassword2')
+        self.browser.find_element(By.XPATH, '//input[@value="login"]').click()
+
+        # create post --> second user is the author
+        self.browser.find_element(By.CLASS_NAME, 'create-button').click()
+        title_input = self.browser.find_element(By.NAME, 'title')
+        title_input.send_keys('test title2')
+        slug_input = self.browser.find_element(By.NAME, 'slug')
+        slug_input.send_keys('test-title2')
+        self.browser.execute_script("tinyMCE.activeEditor.selection.setContent('test content2')")
+        source_input = self.browser.find_element(By.NAME, 'source')
+        source_input.send_keys('http://www.test2.com')
+        tag_input = self.browser.find_element(By.NAME, 'tag')
+        tag_input.send_keys('test_tag2')
+        self.browser.find_element(By.CLASS_NAME, 'create-btn').click()
+
+        title_texts = self.browser.find_elements(By.CLASS_NAME, 'home-title')
+        self.assertEqual('test title2', title_texts[0].text)
+        self.assertEqual('test title', title_texts[1].text)
+
+        author_texts = self.browser.find_elements(By.CLASS_NAME, 'home-author')
+        self.assertIn('test2@test.com', author_texts[0].text)
+        self.assertIn('test@test.com', author_texts[1].text)
+
+    def test_italian_localization(self):
+        # test english localization
+        en_signup_btn = self.browser.find_element(By.CLASS_NAME, 'signup-button').text
+        self.assertEqual('Sign Up', en_signup_btn)
+
+        # change language
+        self.browser.find_element(By.CLASS_NAME, 'lang_btn').click()
+        self.browser.find_element(By.CLASS_NAME, 'each_lang').click()
+
+        # test italian localization
+        it_signup_btn = self.browser.find_element(By.CLASS_NAME, 'signup-button').text
+        self.assertEqual('Registrati', it_signup_btn)
+
+        # test italian localization in search bar
+        ita_search_bar = self.browser.find_element(By.CLASS_NAME, 'search-bar').get_attribute('placeholder')
+        self.assertEqual('Cerca nel sito...', ita_search_bar)
+
+        # change language
+        self.browser.find_element(By.CLASS_NAME, 'lang_btn').click()
+        self.browser.find_element(By.CLASS_NAME, 'each_lang').click()
+
+        # test english localization in search bar
+        en_search_bar = self.browser.find_element(By.CLASS_NAME, 'search-bar').get_attribute('placeholder')
+        self.assertEqual('search the site...', en_search_bar)
